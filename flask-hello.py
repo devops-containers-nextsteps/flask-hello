d85ceb0d751b0a7ddb6f1056e715554ded38f2c9
mmukherjee <mmanas19@gmail.com>
2020-12-02 23:48:48 -0800

    added more text in the api response

--- flask-hello.py
+++ flask-hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Manas is the best in the world'
+    return 'Manas is the best in the world. Lets rock!'
 
 @app.route('/test')
 def test():
